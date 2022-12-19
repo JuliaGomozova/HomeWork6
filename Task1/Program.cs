0d65720d310f8f527afd6860f2526c74f98466d7
Julia Gomozova <sunrain@yandex.ru>
2022-12-19 13:13:37 +0300

    add task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,93 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+*/
+
+//запросить натуральное число
+int GetNaturalNumber (string numStr)
+{
+    bool isNum = false;
+    int num = 0;
+    
+ 
+    while (!isNum)
+    {   
+        if (int.TryParse(numStr, out num) && num > 0)
+            isNum = true;
+        else 
+        {
+            Console.WriteLine("Некорректный ввод числа. Введите число > 0:");
+            numStr = Console.ReadLine() ?? "";
+        }
+    }
+    return num;
+}
+
+//запросить целое число
+int GetNumber (string numStr)
+{
+    bool isNum = false;
+    int num = 0;
+
+    while (!isNum)
+    {   
+        if (int.TryParse(numStr, out num))
+            isNum = true;
+        else 
+        {
+            Console.WriteLine("Некорректный ввод числа. Введите число:");
+            numStr = Console.ReadLine() ?? "";
+        }
+    }
+    return num;
+}
+
+//Ввод массива целых чисел с клавиатуры
+int[] InitUserArray(int length)
+{
+    int[] array = new int[length];
+    int number = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} число:");
+        number = GetNumber(Console.ReadLine() ?? "");
+        array[i] = number;
+    }
+
+    return array;
+}
+
+//распечатать массив на консоль 
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+
+    Console.WriteLine();
+}
+
+//подсчет отрицательных чисел в массиве
+int CounеNegativeInArray(int[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < 0)
+            count ++;
+    }
+    return count;
+}
+
+Console.WriteLine("Сколько чисел Вы хотите ввести?");
+int countNum =  GetNaturalNumber(Console.ReadLine() ?? "");
+Console.WriteLine("");
+
+int[] arrayNum = InitUserArray(countNum);
+Console.WriteLine("");
+PrintArray(arrayNum);
+
+Console.WriteLine($"среди введенных чисел {CounеNegativeInArray(arrayNum)} отрицательных");
